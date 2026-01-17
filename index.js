4a2eaa27fba4086505a472b7e35ae635ba92999a
Olulana Dari <dariolulana@gmail.com>
2026-01-17 22:24:59 +0100

    First Commit, the saloon landing page

---
+++ index.js
@@ -0,0 +1,20 @@
+window.addEventListener("scroll", () => {
+  const nav = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
